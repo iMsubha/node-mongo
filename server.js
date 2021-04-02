0a97a32324aa73f0a95845c57bea3374a21c8a9f
Your Name <subha.fairuz@gmail.com>
2021-04-02 21:18:18 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const bodyParser = require('body-parser')
+app.use(cors());
+app.use(bodyParser.json())
+const PORT =  8080
+//data
+const users = ['subha','fairuz','mahmud','hossain','fahad'];
+
+app.get('/user/:id', (req, res)=>{
+    console.log(req.params.id)
+    const id = req.params.id;
+    const name = users[id];
+    res.send({id,name}); //json 
+});
+
+app.get('/', (req, res)=>{
+   // console.log(req.baseUrl)
+    res.send("I know how to open NODE");
+});
+//Post
+app.post('/addUser', (req, res)=>{
+  //in future, save to database hobe tahole ekta id add hobe
+  //sei id shoho jabe client side a
+  const user = req.body;
+  user.id =55;
+  res.send(user)
+})
+
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`server is running on port ${PORT}`);
+})
